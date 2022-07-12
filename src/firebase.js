c06d8d50cae96caa9e019046654b907f09a85014
doclock4715 <doclock4715@windowslive.com>
2022-07-12 18:19:48 +0300

    web sitesi yayınlama denemeleri 1

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,5 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
@@ -18,7 +17,6 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 export const auth = getAuth();
